4dde4ed3477d3b8b177f5095adf3b455b58c5bc0
danisio <dany_s@abv.bg>
2015-07-26 17:13:15 +0300

    added bottom of the cube

--- scripts/tetris3d.js
+++ scripts/tetris3d.js
@@ -1,6 +1,5 @@
 var Tetris = function () {
-    var initScene, render, renderer, render_stats, physics_stats, scene, camera,
-        ground_material, ground, controls;
+    var renderer, render_stats, physics_stats, scene, camera;
 
     var WIDTH = window.innerWidth,
         HEIGHT = window.innerHeight,
@@ -11,14 +10,10 @@ var Tetris = function () {
         GROUND_HEIGHT = 1,
         BLOCK_MASS = 10,
 
-        GAMEFIELD_WIDTH = 420,
-        GAMEFIELD_HEIGHT = 420,
-        GAMEFIELD_DEPTH = 420,
-
         GAMEFIELD_CONFIG = {
-            width: 420,
-            height: 420,
-            depth: 420,
+            width: 500,
+            height: 500,
+            depth: 500,
 
             segmentWidth: 6,
             segmentHeight: 6,
@@ -34,7 +29,6 @@ var Tetris = function () {
     // Params for Perspective camera
     var VIEW_ANGLE = 30,
         ASPECT = WIDTH / HEIGHT;
-
 
     var CAMERA_POSITION = {
         X: 0,
@@ -73,9 +67,7 @@ var Tetris = function () {
             document.getElementById('viewport').appendChild(physics_stats.domElement);
 
             scene.setGravity(new THREE.Vector3(0, GRAVITY_VECTOR, 0));
-            scene.addEventListener(
-                'update',
-                function () {
+            scene.addEventListener('update', function () {
                     scene.simulate(undefined, 1);
                     physics_stats.update();
                 }
@@ -84,55 +76,34 @@ var Tetris = function () {
 
         // camera.updateProjectionMatrix(); needed only for Orthographic camera
         camera.position.set(CAMERA_POSITION.X, CAMERA_POSITION.Y, CAMERA_POSITION.Z);
-        //camera.position.z = 600;
         camera.lookAt(scene.position);
         scene.add(camera);
-
-
         // End of basic setup
 
         // Ground
-        if (Physijs) {
-            ground_material = Physijs.createMaterial(
-                new THREE.MeshBasicMaterial({color: 0x888899}),
-                .9, // high friction
-                .0 // low restitution
-            );
-            // Next lines are usable only if the ground has a texture, not just color
-            // ground_material.map.wrapS = ground_material.map.wrapT = THREE.RepeatWrapping;
-            // ground_material.map.repeat.set(3, 3);
+        var size = 250, step = BLOCK_SIZE;
+        var geometry = new THREE.Geometry();
 
-            ground = new Physijs.BoxMesh(
-                new THREE.BoxGeometry(420, 1, 420),
-                ground_material,
-                0
-            );
-            ground.receiveShadow = true; // leaving this for now.. hope we can show where the element is going to fall through playing with shadows
-            ground.position.y -= 176;    // If the coordinates are not set, defaults are in the center of the scene.
-            scene.add(ground);
+        for (var i = -size; i <= size; i += step) {
+            geometry.vertices.push(new THREE.Vector3(-size, -size, i));
+            geometry.vertices.push(new THREE.Vector3(size, -size, i));
+            geometry.vertices.push(new THREE.Vector3(i, -size, -size));
+            geometry.vertices.push(new THREE.Vector3(i, -size, size));
         }
 
+        var material = new THREE.LineBasicMaterial({color: 0xA3DA2E, opacity: 0.9, transparent: true});
+        var line = new THREE.Line(geometry, material, THREE.LinePieces);
+        scene.add(line);
+        // end of ground
+
         // Gamefield - the big cube
-
-
         var boundingBoxGeometry = new THREE.BoxGeometry(GAMEFIELD_CONFIG.width, GAMEFIELD_CONFIG.height, GAMEFIELD_CONFIG.depth,
             GAMEFIELD_CONFIG.segmentWidth, GAMEFIELD_CONFIG.segmentHeight, GAMEFIELD_CONFIG.segmentDepth);
         var boundingBox = new THREE.Mesh(
             boundingBoxGeometry,
-            new THREE.MeshBasicMaterial({
-                color: 0x000000,
-                transparent: true,
-                opacity: 0.05
-            }),
-            0
-        );
+            new THREE.MeshBasicMaterial({color: 0x000000, transparent: true, opacity: 0.05}), 0);
 
         scene.add(boundingBox);
-
-        if (Physijs) {
-            scene.simulate();
-            boundingBox.position.y = ground.position.y + GAMEFIELD_CONFIG.width / 2 + 1;
-        }
 
         var outlineMaterial = new THREE.MeshBasicMaterial({
             color: 0xA3DA2E,
@@ -145,6 +116,7 @@ var Tetris = function () {
         outlineMesh.position = boundingBox.position;
         outlineMesh.scale.multiplyScalar(1.01);
         scene.add(outlineMesh);
+        // end of game field
     }
 
     return {
@@ -160,5 +132,4 @@ var Tetris = function () {
         gameFieldConfig: GAMEFIELD_CONFIG,
         collisionObject: COLLISION_OBJECT
     }
-
 }();
